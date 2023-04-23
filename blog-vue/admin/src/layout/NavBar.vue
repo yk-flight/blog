879909fc05b9ad2b8c73087a0c87d595c9f1c120
世纪末的架构师 <1072876976@qq.com>
2023-04-23 16:10:17 +0800

    feat: 完成顶部导航栏右侧工具栏的样式开发

--- blog-vue/admin/src/layout/NavBar.vue
+++ blog-vue/admin/src/layout/NavBar.vue
@@ -2,22 +2,57 @@
   <div class="navbar-container">
     <hamburger class="hamburger-container"></hamburger>
     <breadcrumb class="breadcrumb-container"></breadcrumb>
-    <div class="right-menu">右侧菜单栏</div>
+    <div class="right-menu">
+        <el-tooltip effect="dark" content="Github" placement="bottom">
+          <a :href="GithubUrl" target="__blank">
+            <svg-icon icon="github" class="right-menu-item hover-effect"></svg-icon>
+          </a>
+        </el-tooltip>
+
+        <el-tooltip effect="dark" content="Gitee" placement="bottom">
+          <a :href="GiteeUrl" target="__blank">
+            <svg-icon icon="gitee" class="right-menu-item hover-effect"></svg-icon>
+          </a>
+        </el-tooltip>
+
+        <el-tooltip effect="dark" content="全屏展示" placement="bottom">
+          <screen-full class="right-menu-item hover-effect"></screen-full>
+        </el-tooltip>
+
+      <el-dropdown class="avatar-container right-menu-item hover-effect" trigger="click">
+        <div class="avatar-wrapper">
+          <img src="../assets/images/logo.png" class="user-avatar">
+          <i class="el-icon-caret-bottom" />
+        </div>
+        <el-dropdown-menu slot="dropdown">
+          <router-link to="/user/profile">
+            <el-dropdown-item>个人中心</el-dropdown-item>
+          </router-link>
+          <el-dropdown-item divided>
+            <span>退出登录</span>
+          </el-dropdown-item>
+        </el-dropdown-menu>
+      </el-dropdown>
+    </div>
   </div>
 </template>
 
 <script>
+import ScreenFull from '../components/ScreenFull/index.vue'
 import Hamburger from './components/Hamburger.vue'
 import Breadcrumb from './components/Breadcrumb.vue'
 
 export default {
   name: 'NavBar',
 
-  components: { Hamburger, Breadcrumb },
+  components: { Hamburger, Breadcrumb, ScreenFull },
 
   data () {
     return {
-
+      // 项目Github地址
+      GithubUrl: 'https://gitee.com/dream-flight/blog',
+      // 项目Gitee地址
+      GiteeUrl: 'https://gitee.com/dream-flight/blog'
     }
   },
 
@@ -63,5 +98,49 @@ export default {
   float: right;
   height: 100%;
   line-height: 46px;
+
+  &:focus {
+    outline: none;
+  }
+
+  .right-menu-item {
+    display: inline-block;
+    padding: 0 8px;
+    height: 100%;
+    font-size: 18px;
+    color: #5a5e66;
+    vertical-align: text-bottom;
+
+    &.hover-effect {
+      cursor: pointer;
+      transition: background .3s;
+      &:hover {
+        background: rgba(0, 0, 0, .025)
+      }
+    }
+  }
+
+  .avatar-container {
+    margin-right: 30px;
+
+    .avatar-wrapper {
+      margin-top: 5px;
+      position: relative;
+      .user-avatar {
+        cursor: pointer;
+        width: 40px;
+        height: 40px;
+         border-radius: 10px;
+      }
+
+      .el-icon-caret-bottom {
+        cursor: pointer;
+        position: absolute;
+        right: -20px;
+        top: 25px;
+        font-size: 12px;
+      }
+    }
+  }
 }
 </style>
