5a921f4475e511b0a4e32a12900b259c027a2290
世纪末的架构师 <1072876976@qq.com>
2023-04-27 07:55:38 +0800

    feat: 创建菜单组件

---
+++ blog-vue/admin/src/views/monitor/operate-log/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    系统监控内容
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'OperateLog',
+
+  data () {
+    return {
+
+    }
+  },
+
+  mounted () {
+
+  },
+
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
