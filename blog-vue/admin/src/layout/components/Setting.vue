ffa9789c8d33264df06c919f7959676b876a0483
世纪末的架构师 <1072876976@qq.com>
2023-04-29 22:25:11 +0800

    refactor: 重构系统菜单结构

--- blog-vue/admin/src/layout/components/Setting.vue
+++ blog-vue/admin/src/layout/components/Setting.vue
@@ -9,10 +9,24 @@
         <!-- 暗色主题 -->
         <div class="setting-drawer-block-checbox-item" @click="handleTheme('theme-dark')">
           <img src="@/assets/images/dark.svg" alt="dark" />
+          <div v-if="sideTheme === 'theme-dark'" class="setting-drawer-block-checbox-selectIcon" style="display: block;">
+            <i aria-label="图标: check" class="anticon anticon-check">
+              <svg viewBox="64 64 896 896" data-icon="check" width="1em" height="1em" :fill="theme" aria-hidden="true" focusable="false" class="">
+                 <path d="M912 190h-69.9c-9.8 0-19.1 4.5-25.1 12.2L404.7 724.5 207 474a32 32 0 0 0-25.1-12.2H112c-6.7 0-10.4 7.7-6.3 12.9l273.9 347c12.8 16.2 37.4 16.2 50.3 0l488.4-618.9c4.1-5.1.4-12.8-6.3-12.8z"/>
+              </svg>
+            </i>
+          </div>
         </div>
         <!-- 亮色主题 -->
         <div class="setting-drawer-block-checbox-item" @click="handleTheme('theme-light')">
           <img src="@/assets/images/light.svg" alt="light" />
+          <div v-if="sideTheme === 'theme-light'" class="setting-drawer-block-checbox-selectIcon" style="display: block;">
+            <i aria-label="图标: check" class="anticon anticon-check">
+              <svg viewBox="64 64 896 896" data-icon="check" width="1em" height="1em" :fill="theme" aria-hidden="true" focusable="false" class="">
+                <path d="M912 190h-69.9c-9.8 0-19.1 4.5-25.1 12.2L404.7 724.5 207 474a32 32 0 0 0-25.1-12.2H112c-6.7 0-10.4 7.7-6.3 12.9l273.9 347c12.8 16.2 37.4 16.2 50.3 0l488.4-618.9c4.1-5.1.4-12.8-6.3-12.8z"/>
+              </svg>
+            </i>
+          </div>
         </div>
       </div>
     </div>
@@ -93,7 +107,9 @@ export default {
   data () {
     return {
       // 侧边栏主题
-      sideTheme: this.$store.getters.sideTheme
+      sideTheme: this.$store.getters.sideTheme,
+      // 系统主题色
+      theme: this.$store.state.settings.theme
     }
   },
 
@@ -182,10 +198,10 @@ export default {
         position: absolute;
         top: 0;
         right: 0;
+        left: 22px;
         width: 100%;
         height: 100%;
         padding-top: 15px;
-        padding-left: 24px;
         color: #1890ff;
         font-weight: 700;
         font-size: 14px;
