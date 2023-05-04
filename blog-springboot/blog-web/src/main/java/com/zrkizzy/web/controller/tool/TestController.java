d60645178afd224efdac62df3dde843c911fc8a0
世纪末的架构师 <1072876976@qq.com>
2023-05-04 23:26:12 +0800

    feat: 测试发送邮件功能成功

--- blog-springboot/blog-web/src/main/java/com/zrkizzy/web/controller/tool/TestController.java
+++ blog-springboot/blog-web/src/main/java/com/zrkizzy/web/controller/tool/TestController.java
@@ -1,9 +1,16 @@
 package com.zrkizzy.web.controller.tool;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.mail.MessagingException;
+import java.util.Date;
 
 /**
  * 系统创建测试类
@@ -16,9 +23,37 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/test")
 public class TestController {
 
+    @Autowired
+    private JavaMailSender javaMailSender;
+
     @GetMapping("/hello")
     public String hello() {
         return "Hello, World!";
     }
 
+    @ApiOperation("测试发送邮件")
+    @GetMapping("/email")
+    public void sendEmail() {
+        try {
+            //true 代表支持复杂的类型
+            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(javaMailSender.createMimeMessage(),true);
+            //邮件发信人
+            mimeMessageHelper.setFrom("1072876976@qq.com");
+            //邮件收信人
+            mimeMessageHelper.setTo("2675525537@qq.com");
+            //邮件主题
+            mimeMessageHelper.setSubject("测试邮件发送功能");
+            //邮件内容
+            mimeMessageHelper.setText("当你看到我的时候就已经成功了");
+            //邮件发送时间
+            mimeMessageHelper.setSentDate(new Date());
+            //发送邮件
+            javaMailSender.send(mimeMessageHelper.getMimeMessage());
+            System.out.println("发送邮件成功");
+
+        } catch (MessagingException e) {
+            e.printStackTrace();
+            System.out.println("发送邮件失败："+e.getMessage());
+        }
+    }
 }
