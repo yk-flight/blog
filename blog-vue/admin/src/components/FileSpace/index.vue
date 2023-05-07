11ecbc02c39a3871adf769b2ba28219b3478aa74
世纪末的架构师 <1072876976@qq.com>
2023-05-07 23:11:13 +0800

    feat: 自定义前端Message消息提示样式

---
+++ blog-vue/admin/src/components/FileSpace/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <el-dialog title="文件空间"></el-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FileSpace',
+
+  data () {
+    return {
+
+    }
+  },
+
+  mounted () {
+
+  },
+
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
