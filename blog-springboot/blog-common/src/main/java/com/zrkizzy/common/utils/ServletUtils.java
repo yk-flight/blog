89df974fdc99e4023b412d9085eccd378f8a816b
世纪末的架构师 <1072876976@qq.com>
2023-06-20 21:34:13 +0800

    feat: 增加获取Servlet请求与响应工具类

---
+++ blog-springboot/blog-common/src/main/java/com/zrkizzy/common/utils/ServletUtils.java
@@ -0,0 +1,45 @@
+package com.zrkizzy.common.utils;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 请求响应工具类
+ *
+ * @author zhangrongkang
+ * @since 2023/6/20
+ */
+@Component
+public class ServletUtils {
+
+    /**
+     * 获取当前线程请求
+     *
+     * @return request请求
+     */
+    public static HttpServletRequest getRequest() {
+        return getRequestAttributes().getRequest();
+    }
+
+    /**
+     * 获取当前线程响应
+     *
+     * @return request请求
+     */
+    public static HttpServletResponse getResponse() {
+        return getRequestAttributes().getResponse();
+    }
+
+    /**
+     * 获取当前线程中的Servlet相关属性
+     *
+     * @return Servlet相关属性
+     */
+    private static ServletRequestAttributes getRequestAttributes() {
+        return (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+    }
+}
