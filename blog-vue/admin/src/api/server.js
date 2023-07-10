e2c6c8a8939fb8bf550ee7149899431ef0e61920
世纪末的架构师 <1072876976@qq.com>
2023-07-10 08:26:19 +0800

    feat: 封装前端获取服务监控信息API

---
+++ blog-vue/admin/src/api/server.js
@@ -0,0 +1,11 @@
+import request from '../utils/request'
+
+/**
+ * 获取服务监控信息
+ */
+export const getMonitorInfo = () => {
+  return request({
+    url: '/admin/service-monitor/getMonitorInfo',
+    method: 'GET',
+  })
+}
