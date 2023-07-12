6b1c70693e61f0680f927450e1fa8c236f4fbc25
世纪末的架构师 <1072876976@qq.com>
2023-07-12 19:02:52 +0800

    feat: 在线用户前端页面开发

--- blog-vue/admin/src/views/monitor/online/index.vue
+++ blog-vue/admin/src/views/monitor/online/index.vue
@@ -5,17 +5,13 @@
       <el-row class="search-container" type="flex">
         <el-col :span="24" :xs="24">
           <el-form size="small" :inline="true" v-show="showSearch" label-width="68px" :model="queryParams" ref="queryForm">
-            <!-- 请求时间 -->
-            <el-form-item label="登录时间" size="small">
-              <el-date-picker
-                v-model="queryParams.dataRange"
-                class="search-item"
-                value-format="yyyy-MM-dd"
-                type="daterange"
-                range-separator="-"
-                start-placeholder="开始日期"
-                end-placeholder="结束日期"
-              ></el-date-picker>
+            <!-- 用户名 -->
+            <el-form-item label="用户名">
+              <el-input v-model="queryParams.username" class="search-item" placeholder="请输入用户名" size="small" clearable></el-input>
+            </el-form-item>
+            <!-- 用户昵称 -->
+            <el-form-item label="用户昵称">
+              <el-input v-model="queryParams.nickname" class="search-item" placeholder="请输入用户昵称" size="small" clearable></el-input>
             </el-form-item>
             <!-- 搜索按钮 -->
             <el-form-item>
@@ -25,17 +21,31 @@
           </el-form>
         </el-col>
       </el-row>
+
+      <el-row :gutter="10" class="button-container">
+        <!-- :columns="columns" @getTableData="getTableData" -->
+        <right-toolbar :showSearch.sync="showSearch"></right-toolbar>
+      </el-row>
+
+      <el-table v-loading="loading" element-loading-text="正在加载表格数据" :data="tableData" border>
+        <template slot="empty">
+          <el-empty :image-size="200"></el-empty>
+        </template>
+        <!-- 登录编号 -->
+        <el-table-column prop="track" label="登录编号" align="center"></el-table-column>
+      </el-table>
     </div>
   </div>
 </template>
 
 <script>
 import PageTitle from '../../../components/PageTitle/index.vue'
+import RightToolbar from '../../../components/RightToolbar/index.vue'
 
 export default {
   name: 'Online',
 
-  components: { PageTitle },
+  components: { PageTitle, RightToolbar },
 
   data () {
     return {
@@ -51,9 +61,15 @@ export default {
         currentPage: 1,
         // 页面大小
         pageSize: 10,
-        // 时间范围
-        dataRange: []
-      }
+        // 用户名
+        username: undefined,
+        // 用户昵称
+        nickname: undefined
+      },
+      // 数据表格等待框
+      loading: false,
+      // 表格数据
+      tableData: []
     }
   },
 
@@ -65,6 +81,7 @@ export default {
   methods: {
     // 点击查询按钮
     handleQuery () {
+      this.queryParams.currentPage = 1
     },
     // 点击重置按钮
     handleReset () {
