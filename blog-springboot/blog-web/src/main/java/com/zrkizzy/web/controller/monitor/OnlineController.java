eedc6ea0e3f5b55c60326e67668f310e04182a7d
世纪末的架构师 <1072876976@qq.com>
2023-07-12 23:58:26 +0800

    feat: 完成下线用户接口开发并更新SQL文件

--- blog-springboot/blog-web/src/main/java/com/zrkizzy/web/controller/monitor/OnlineController.java
+++ blog-springboot/blog-web/src/main/java/com/zrkizzy/web/controller/monitor/OnlineController.java
@@ -1,17 +1,18 @@
 package com.zrkizzy.web.controller.monitor;
 
+import com.zrkizzy.common.annotation.OperateLogAnnotation;
 import com.zrkizzy.common.base.response.PageResult;
 import com.zrkizzy.common.base.response.Result;
+import com.zrkizzy.common.service.IRedisService;
 import com.zrkizzy.data.query.monitor.OnlineQuery;
 import com.zrkizzy.data.vo.monitor.OnlineUserVO;
 import com.zrkizzy.server.service.monitor.IOnlineService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import static com.zrkizzy.common.constant.RedisConst.USER_PREFIX;
 
 /**
  * 在线用户控制器
@@ -25,11 +26,22 @@ import org.springframework.web.bind.annotation.RestController;
 public class OnlineController {
 
     @Autowired
+    private IRedisService redisService;
+    @Autowired
     private IOnlineService onlineService;
 
     @ApiOperation("分页获取在线用户")
     @PostMapping("/list")
     public Result<PageResult<OnlineUserVO>> listOnline(@RequestBody OnlineQuery onlineQuery) {
         return Result.success(onlineService.listOnlineUsers(onlineQuery));
+    }
+
+    @ApiOperation("下线指定用户")
+    @OperateLogAnnotation(type = "3")
+    @DeleteMapping("/offline/{track}")
+    public Result<?> offlineUser(@PathVariable String track) {
+        // 下线指定用户
+        redisService.del(USER_PREFIX + track);
+        return Result.success();
     }
 }
