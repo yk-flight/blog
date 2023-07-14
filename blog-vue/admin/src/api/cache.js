b3d77854ceae7a2b9ce595d1b58732846f738be4
世纪末的架构师 <1072876976@qq.com>
2023-07-14 14:58:52 +0800

    feat: 完成获取缓存列表、指定缓存、刷新缓存列表前端开发

--- blog-vue/admin/src/api/cache.js
+++ blog-vue/admin/src/api/cache.js
@@ -3,9 +3,29 @@ import request from '../utils/request'
 /**
  * 获取所有Redis缓存键类型
  */
-export const listRedisKeyType = () => {
+export const listCacheType = () => {
   return request({
-    url: '/admin/cache/listRedisKeyType',
+    url: '/admin/cache/listCacheType',
+    method: 'GET'
+  })
+}
+
+/**
+ * 获取所有缓存键
+ */
+export const listCacheKeys = (type) => {
+  return request({
+    url: `/admin/cache/listCacheKeys/${type}`,
+    method: 'GET'
+  })
+}
+
+/**
+ * 获取指定缓存信息
+ */
+export const getCacheInfoByKey = (key) => {
+  return request({
+    url: `/admin/cache/getCacheInfoByKey/${key}`,
     method: 'GET'
   })
 }
