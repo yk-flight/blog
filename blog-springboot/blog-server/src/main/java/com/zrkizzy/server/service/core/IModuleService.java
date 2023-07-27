02568c5797bf2226251a3488bf68998bb9a1f11e
世纪末的架构师 <1072876976@qq.com>
2023-07-27 21:02:54 +0800

    feat: 资源模块基本功能开发完成

--- blog-springboot/blog-server/src/main/java/com/zrkizzy/server/service/core/IModuleService.java
+++ blog-springboot/blog-server/src/main/java/com/zrkizzy/server/service/core/IModuleService.java
@@ -1,6 +1,10 @@
 package com.zrkizzy.server.service.core;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.zrkizzy.common.base.response.OptionsVO;
+import com.zrkizzy.data.domain.Module;
+import com.zrkizzy.data.dto.ModuleDTO;
+import com.zrkizzy.data.query.ModuleQuery;
 
 import java.util.List;
 
@@ -18,4 +22,37 @@ public interface IModuleService {
      * @return 模块选项集合
      */
     List<OptionsVO> listModuleOptions();
+
+    /**
+     * 分页获取资源模块
+     *
+     * @param moduleQuery 资源模块查询对象
+     * @return 资源模块分页数据
+     */
+    Page<Module> listModules(ModuleQuery moduleQuery);
+
+    /**
+     * 添加或更新资源模块
+     *
+     * @param moduleDTO 资源模块数据接收对象
+     * @return 是否添加/更新成功
+     */
+    Boolean saveModule(ModuleDTO moduleDTO);
+
+    /**
+     * 获取指定资源模块信息
+     *
+     * @param moduleId 资源模块ID
+     * @return 资源模块对象
+     */
+    Module getModuleById(Long moduleId);
+
+    /**
+     * 批量删除资源模块数据
+     *
+     * @param ids 资源模块ID
+     * @return true：删除成功，false：删除失败
+     */
+    Boolean deleteBatch(List<Long> ids);
+
 }
--- blog-springboot/blog-web/src/main/java/com/zrkizzy/web/controller/core/ModuleController.java
+++ blog-springboot/blog-web/src/main/java/com/zrkizzy/web/controller/core/ModuleController.java
@@ -1,9 +1,14 @@
 package com.zrkizzy.web.controller.core;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.zrkizzy.common.base.response.OptionsVO;
 import com.zrkizzy.common.base.response.PageResult;
 import com.zrkizzy.common.base.response.Result;
+import com.zrkizzy.common.enums.HttpStatusEnum;
+import com.zrkizzy.common.utils.bean.BeanCopyUtil;
+import com.zrkizzy.data.domain.Module;
 import com.zrkizzy.data.dto.ModuleDTO;
+import com.zrkizzy.data.query.ModuleQuery;
 import com.zrkizzy.data.vo.ModuleVO;
 import com.zrkizzy.server.service.core.IModuleService;
 import io.swagger.annotations.Api;
@@ -34,8 +39,34 @@ public class ModuleController {
 
     @ApiOperation("分页查询资源模块")
     @PostMapping("/list")
-    public Result<PageResult<ModuleVO>> list(@RequestBody ModuleDTO moduleDTO) {
-        return null;
+    public Result<PageResult<ModuleVO>> listModules(@RequestBody ModuleQuery moduleQuery) {
+        Page<Module> modulePage = moduleService.listModules(moduleQuery);
+        return Result.success(PageResult.<ModuleVO>builder()
+                .list(BeanCopyUtil.copyList(modulePage.getRecords(), ModuleVO.class))
+                .total(modulePage.getTotal())
+                .build());
+    }
+
+    @ApiOperation("添加-更新资源模块")
+    @PostMapping("/save")
+    public Result<Boolean> saveModule(@RequestBody ModuleDTO moduleDTO) {
+        // 保存资源模块数据
+        return Result.success(moduleService.saveModule(moduleDTO));
+    }
+
+    @ApiOperation("获取指定资源模块信息")
+    @GetMapping("/getModuleById/{moduleId}")
+    public Result<ModuleVO> getModuleById (@PathVariable Long moduleId) {
+        return Result.success(BeanCopyUtil.copy(moduleService.getModuleById(moduleId), ModuleVO.class));
+    }
+
+    @ApiOperation("批量删除资源模块数据")
+    @DeleteMapping("/delete")
+    public Result<?> delete(@RequestBody List<Long> ids) {
+        if (moduleService.deleteBatch(ids)) {
+            return Result.success();
+        }
+        return Result.failure(HttpStatusEnum.INTERNAL_SERVER_ERROR, "资源模块数据删除失败");
     }
 
 }
