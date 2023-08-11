c97e3b6acdc7454da36844b54624ec082805b32d
世纪末的架构师 <1072876976@qq.com>
2023-08-11 14:50:38 +0800

    feat: 博客前端导入动画样式依赖

--- blog-vue/blog/src/layout/SideBar/components/Phone.vue
+++ blog-vue/blog/src/layout/SideBar/components/Phone.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="phone-container">
-    <div class="phone-header">
+  <div class="wow animated slideInRight phone-container">
+
+    <!-- 手机头部 -->
+    <div class="phone-header" ref="phoneRef">
       <span style="width: 25%;">00:31</span>
       <div class="island"></div>
       <div class="icon">
@@ -8,6 +10,11 @@
         <svg-icon icon="wifi"></svg-icon>
         <svg-icon icon="battery"></svg-icon>
       </div>
+    </div>
+
+    <!-- 手机内部样式 -->
+    <div class="phone-body">
+
     </div>
 
     <div class="phone-footer">
@@ -22,12 +29,15 @@ export default {
 
   data () {
     return {
-
+      // 是否显示动画
+      showAnimation: false,
+      // 当前的视觉高度
+      currentScroll: 0
     }
   },
 
   mounted () {
-
+    this.$wow.init()
   },
 
   methods: {
@@ -45,6 +55,7 @@ export default {
   border-radius: 36px;
   box-shadow: 0 0 2px #000;
   transition: box-shadow 0.3s ease;
+  // animation: slideInFromRight 1s ease-in-out forwards;
 }
 .phone-container:hover {
   box-shadow: 1px 2px 10px #000;
@@ -58,31 +69,49 @@ export default {
   display: flex;
   justify-content: space-between;
   align-items: center;
-}
-.island {
+
+  .island {
   width: 45%;
   height: 1.25rem;
   background-color: #000;
   border-radius: 10px;
-}
-.icon {
-  width: 25%;
-  .svg-icon {
-    margin-left: 4px;
+  }
+  .icon {
+    width: 25%;
+    .svg-icon {
+      margin-left: 4px;
+    }
   }
 }
+
+.phone-body {
+  width: 100%;
+  height: 30rem;
+  padding: 10px;
+}
+
 .phone-footer {
   // position: relative;
-  margin-top: 29.625rem;
+  // margin-top: 4.625rem;
   display: flex;
   justify-content: center;
   width: 100%;
 
   .home {
-    height: 5px;
+    height: 0.3rem;
     width: 120px;
     border-radius: 3px;
     background-color: #fff;
+  }
+}
+@keyframes slideInFromRight {
+  0% {
+    transform: translateX(100%);
+    opacity: 0;
+  }
+  100% {
+    transform: translateX(0);
+    opacity: 1;
   }
 }
 </style>
--- blog-vue/blog/src/main.js
+++ blog-vue/blog/src/main.js
@@ -6,10 +6,29 @@ import vuetify from './plugins/vuetify'
 import SvgIcons from './icons'
 
 import './styles/style.scss'
+import 'animate.css'
+
+import { WOW } from 'wowjs'
+import animated from 'wowjs/css/libs/animate.css'
 
 Vue.config.productionTip = false
+// 创建全局实例
+Vue.prototype.$wow = new WOW({
+  // 需要执行动画的元素的 class
+  boxClass: 'wow',
+  // animation.css 动画的 class
+  animateClass: 'animated',
+  // 距离可视区域多少开始执行动画
+  offset: 200,
+  // 是否在移动设备上执行动画
+  mobile: true,
+  // 异步加载的内容是否有效
+  live: true
+}
+)
 
 Vue.use(SvgIcons)
+Vue.use(animated)
 
 new Vue({
   router,
